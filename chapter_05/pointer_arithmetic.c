0496ff291c90aab45a6bab6522920ac43bc258b4
Debasish Sarker <debasishsarker36@gmail.com>
2023-08-21 17:22:03 +0600

    pattern matching

---
+++ chapter_05/pointer_arithmetic.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+int main()
+{ 
+	/*int arr[3][5] = {
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15}
+	};
+
+	int *arr_ptr = arr[0];
+	
+	printf("P --> %d \n", *(arr_ptr + 0*5 + 2));
+	printf("P --> %d \n", *(arr_ptr + 1*5 + 2));
+	printf("P --> %d \n", *(arr_ptr + 2*5 + 2));
+	//printf("P --> %lld \n", (long long) arr_ptr++); */
+
+	char arr[10];
+
+	int i;
+	for(i = 0; i<10; i++)
+		printf("%d\n", arr[i]);
+	
+	
+	return 0;
+}
+ 
+
+
+//long htoi()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
